ea7bd01f494b70a1ec54924d6a13a87f4b2e7ee4
Claire Longo <longoclaire@gmail.com>
2018-09-11 09:45:24 -0600

    working flask app for ensemble movie recs

--- app.py
+++ app.py
@@ -1,37 +1,44 @@
-from flask import Flask, render_template
-from flask_restful import reqparse, Api, Resource
 import pickle
 import pandas as pd
-
+from src.similarity import SimilarityModel
+from flask import Flask
+from flask_restful import reqparse, Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
-# load similarity matrix
-similarity_matrix = pd.read_csv("data//models/similarity_matrix.pkl")
 
+#load enbeddings and movie details
+text_embeddings = pd.read_pickle('data/autoencoder_embeddings.pkl')
+text_embeddings = pd.DataFrame(text_embeddings)
+ratings_embeddings = pd.read_pickle('data/movie_embeddings_1.pkl')
+movies = pd.read_csv('data/movie_demographics.csv', index_col=0)
+
+#initialize similarity model
+sm = SimilarityModel()
 
 # parse payload
 parser = reqparse.RequestParser()
-parser.add_argument('movie')
-
-@app.route("/")
-def main():
-    return render_template('index.html')
-
-@app.route('/showSignUp')
-def showSignUp():
-    return render_template('signup.html')
-
+parser.add_argument('movie_title')
 
 class MovieSimilarity(Resource):
     def get(self):
-        seed_movie = parser.parse_args()
-        output = {'movie': ['A','B','C'], 'similarity_score': [0,0,0]}
+
+        #get movie id from title
+        #TODO update this to a string matching algorithm that finds the id from the string you typed.
+        seed_title = parser.parse_args().movie_title
+        seed_id = movies.query('title == @seed_title').index.values[0]
+
+        #get similarity from the collaborative recommender
+        collaborative_similarity = sm.get_similar_movies_from_embeddings(seed_id, ratings_embeddings, movies)
+
+        #get similarity from the collaborative recommender
+        content_similarity = sm.get_similar_movies_from_embeddings(seed_id, text_embeddings, movies)
+
+        #ensemble similarity scores and return top 20 most similar movies
+        output = sm.ensemble_similarity_scores(collaborative_similarity, content_similarity)
         return output
 
-
 api.add_resource(MovieSimilarity, '/')
-
 
 if __name__ == '__main__':
     app.run(debug=False)
