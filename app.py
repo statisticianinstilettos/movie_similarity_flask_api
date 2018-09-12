bb8a5730b1e574bec315605bf2f07cc2b4c19a6e
Claire Longo <longoclaire@gmail.com>
2018-09-12 14:24:52 -0600

    add neural net method

--- app.py
+++ app.py
@@ -8,8 +8,10 @@ api = Api(app)
 
 
 #load enbeddings and movie details
-text_embeddings = pd.read_csv('data/svd_text_embeddings.csv', index_col=0)
-ratings_embeddings = pd.read_csv('data/svd_ratings_embeddings.csv', index_col=0)
+svd_text_embeddings = pd.read_csv('data/svd_text_embeddings.csv', index_col=0)
+svd_ratings_embeddings = pd.read_csv('data/svd_ratings_embeddings.csv', index_col=0)
+nn_text_embeddings = pd.read_csv('data/nn_text_embeddings.csv', index_col=0)
+nn_ratings_embeddings = pd.read_csv('data/nn_ratings_embeddings.csv', index_col=0)
 movies = pd.read_csv('data/movie_demographics.csv', index_col=0)
 
 #initialize similarity model
@@ -18,20 +20,29 @@ sm = SimilarityModel()
 #parse payload
 parser = reqparse.RequestParser()
 parser.add_argument('movie_title')
+parser.add_argument('model')
 
 class MovieSimilarity(Resource):
     def get(self):
-
         #get movie id from title
         #TODO update this to a string matching algorithm that finds the id from the string you typed.
         seed_title = parser.parse_args().movie_title
         seed_id = movies.query('title == @seed_title').index.values[0]
 
-        #get similarity from the collaborative recommender
-        collaborative_similarity = sm.get_similar_movies_from_embeddings(seed_id, ratings_embeddings, movies)
+        #get model type
+        model = parser.parse_args().model
+        assert model in ['matrix factorization', 'neural network'], "model must be svd or neural_network"
 
-        #get similarity from the collaborative recommender
-        content_similarity = sm.get_similar_movies_from_embeddings(seed_id, text_embeddings, movies)
+        if model == "matrix factorization":
+            #get similarity from the collaborative recommender
+            collaborative_similarity = sm.get_similar_movies_from_embeddings(seed_id, svd_ratings_embeddings, movies)
+            #get similarity from the collaborative recommender
+            content_similarity = sm.get_similar_movies_from_embeddings(seed_id, svd_text_embeddings, movies)
+        if model == "neural network":
+            #get similarity from the collaborative recommender
+            collaborative_similarity = sm.get_similar_movies_from_embeddings(seed_id, nn_ratings_embeddings, movies)
+            #get similarity from the collaborative recommender
+            content_similarity = sm.get_similar_movies_from_embeddings(seed_id, nn_text_embeddings, movies)
 
         #ensemble similarity scores and return top 20 most similar movies
         output = sm.ensemble_similarity_scores(collaborative_similarity, content_similarity)
